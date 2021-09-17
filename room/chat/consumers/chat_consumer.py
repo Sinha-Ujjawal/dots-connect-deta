93f5b414b0054548f1c5742106984b15092af3b0
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2021-09-17 21:57:45 +0530

    1. Made consumers asynchronous

--- room/chat/consumers/chat_consumer.py
+++ room/chat/consumers/chat_consumer.py
@@ -1,34 +1,32 @@
-import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import JsonWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
-class ChatConsumer(JsonWebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
         route_kwargs = self.scope["url_route"]["kwargs"]
         self.room_name = route_kwargs["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
         # join the room
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name,
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, _close_code):
+    async def disconnect(self, _close_code):
         # leave the group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name,
         )
 
-    def receive_json(self, json_data):
+    async def receive_json(self, json_data):
         message = json_data["message"]
 
         # send message to the group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "chat_message",
@@ -36,8 +34,8 @@ class ChatConsumer(JsonWebsocketConsumer):
             },
         )
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event["message"]
 
         # send message to WebSocket
-        self.send_json(content={"message": message})
+        await self.send_json(content={"message": message})
