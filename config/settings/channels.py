969d3b48db892b13be96496bb9a6680f26ee6b40
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2021-09-17 21:50:56 +0530

    1. Working Chat App with redis channel 2. Currently the webscoket is Synchronous 3. Need to change that to Async

--- config/settings/channels.py
+++ config/settings/channels.py
@@ -1,1 +1,17 @@
+from .env_reader import env
+
 ASGI_APPLICATION = "config.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": env.list(
+                "DJANGO_CHANNEL_HOSTS",
+                default=[
+                    ("127.0.0.1", 6379),  # default localhost @ 6379
+                ],
+            ),
+        },
+    },
+}
--- room/chat/consumers/chat_consumer.py
+++ room/chat/consumers/chat_consumer.py
@@ -1,14 +1,43 @@
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import JsonWebsocketConsumer
 
 
 class ChatConsumer(JsonWebsocketConsumer):
     def connect(self):
+        route_kwargs = self.scope["url_route"]["kwargs"]
+        self.room_name = route_kwargs["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+
+        # join the room
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name,
+        )
+
         self.accept()
 
     def disconnect(self, _close_code):
-        pass
+        # leave the group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name,
+        )
 
     def receive_json(self, json_data):
         message = json_data["message"]
-        self.send_json(content={"message": message[::-1]})
+
+        # send message to the group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+            },
+        )
+
+    def chat_message(self, event):
+        message = event["message"]
+
+        # send message to WebSocket
+        self.send_json(content={"message": message})
