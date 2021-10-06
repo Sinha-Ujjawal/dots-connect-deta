42bf2477a2b6d5ee165ad83680daf9e7fe49fc7c
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2021-10-06 22:46:42 +0530

    broadcasted game_ids will be sorted by join timestamp

--- dots_connect_deta/board/consumers/game_consumer.py
+++ dots_connect_deta/board/consumers/game_consumer.py
@@ -1,4 +1,5 @@
 from typing import List, Optional, Set
+from datetime import datetime
 import logging
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
@@ -9,16 +10,28 @@ from ..models import Room
 from ..selectors import room_get_by_id
 
 
+@dataclass(frozen=True)
+class UserInfo:
+    game_id: uuid.UUID
+    timestamp: datetime = field(default_factory=datetime.now)
+
+
 @dataclass
 class ChannelData:
-    game_ids: Set[uuid.UUID] = field(default_factory=set)
+    user_infos: Set[UserInfo] = field(default_factory=set)
 
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
-class GameIdResponse:
+class UserIdResponse:
     game_id: str
+    timestamp: str
     me: bool
+
+    def __init__(self, user_info: UserInfo, me: bool):
+        self.game_id = user_info.game_id.hex
+        self.timestamp = str(user_info.timestamp)
+        self.me = me
 
 
 class GameConsumer(AsyncJsonWebsocketConsumer):
@@ -27,18 +40,18 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
 
         self.room_id = route_kwargs["room_id"]
         self.room_group_name = f"chat_{self.room_id}"
-        self.game_id = uuid.uuid4()
+        self.user_info = UserInfo(game_id=uuid.uuid4())
 
         self.room: Optional[Room] = await self.get_room_by_id()
         if self.room:  # existing room
-            self.register_game_id()
+            self.register_user_info()
             # join the group
             await self.channel_layer.group_add(
                 self.room_group_name,
                 self.channel_name,
             )
             await self.accept()
-            await self.game_ids_broadcast_signal()
+            await self.user_infos_broadcast_signal()
         else:
             await self.close()
 
@@ -59,31 +72,33 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         else:
             return channel_data
 
-    def register_game_id(self):
+    def register_user_info(self):
         channel_data = self.get_channel_data()
-        channel_data.game_ids.add(self.game_id)
+        channel_data.user_infos.add(self.user_info)
         self.update_channel_data(channel_data=channel_data)
 
-    def unregister_game_id(self):
+    def unregister_user_info(self):
         channel_data = self.get_channel_data()
-        channel_data.game_ids.discard(self.game_id)
+        channel_data.user_infos.discard(self.user_info)
         self.update_channel_data(channel_data=channel_data)
 
-    def get_game_ids(self) -> List[GameIdResponse]:
+    def get_user_infos(self) -> List[UserIdResponse]:
         channel_data = self.get_channel_data()
         return [
-            GameIdResponse(game_id=game_id.hex, me=game_id == self.game_id)
-            for game_id in channel_data.game_ids
+            UserIdResponse(user_info=user_info, me=user_info == self.user_info)
+            for user_info in sorted(
+                channel_data.user_infos, key=lambda user_info: user_info.timestamp
+            )
         ]
 
     async def disconnect(self, _close_code):
-        self.unregister_game_id()
+        self.unregister_user_info()
         # leave the group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name,
         )
-        await self.game_ids_broadcast_signal()
+        await self.user_infos_broadcast_signal()
 
     async def receive_json(self, json_data):
         message = json_data["message"]
@@ -102,7 +117,7 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         # send message to WebSocket
         await self.send_json(content={"message": message})
 
-    async def game_ids_broadcast_signal(self):
+    async def user_infos_broadcast_signal(self):
         logging.info("Broadcasting Game Ids")
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -114,5 +129,5 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
 
     async def game_ids_broadcast_handler(self, _event):
         await self.send_json(
-            content=[gameIdRes.to_dict() for gameIdRes in self.get_game_ids()]
+            content=[gameIdRes.to_dict() for gameIdRes in self.get_user_infos()]
         )
